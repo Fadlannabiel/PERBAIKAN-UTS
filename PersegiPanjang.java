ec4811d29049ca89bcf29af49ef64a9252e92ffc
Fadlannabiel <115559953+Fadlannabiel@users.noreply.github.com>
2022-12-01 22:01:43 +0700

    Create PersegiPanjang.java

---
+++ PersegiPanjang.java
@@ -0,0 +1,27 @@
+package BangunDatar;
+import BangunDatar.BangunDatar;
+/**
+ *
+ * @author User
+ */
+    public class PersegiPanjang implements BangunDatar{
+    double panjang;
+    double lebar;
+    double luasPersegiPanjang;
+
+    public PersegiPanjang(double panjang,double lebar){
+        this.panjang=panjang;
+        this.lebar=lebar;
+    }
+
+    @Override
+    public void luas() {
+        luasPersegiPanjang=this.panjang*this.lebar;
+    }
+
+    @Override
+    public double getLuas(){
+        return luasPersegiPanjang;
+    }
+    
+}
